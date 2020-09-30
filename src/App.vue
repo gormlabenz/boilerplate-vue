7def06272d3c21c36bd5ed3e901172101c8e2774
Gorm Labenz <gormlabenz@gorms-mbp.localdomain>
2020-09-30 13:56:14 +0200

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p class=" text-6xl">Hello You</p>
+    <HelloWorld class="" msg="Welcome to Your Vue.js App" />
+    <div
+      class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ..."
+    >
+      Test
+    </div>
   </div>
 </template>
 
